4dd55445ad58adb91c4d2501342f6bb1738432cf
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-07-01 12:35:03 +0530

    Inital Kafka Larning structure

---
+++ src/main/java/com/javakafka/kafkaproducerexample/service/kafkaMessagePublisher.java
@@ -0,0 +1,30 @@
+package com.javakafka.kafkaproducerexample.service;
+
+import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class kafkaMessagePublisher {
+
+    public kafkaMessagePublisher(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    private final KafkaTemplate<String,Object> kafkaTemplate ;
+
+    public void sendMessageToTopic (String message){
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send("Java-demo1", message);
+        future.whenComplete((result,ex)->{
+            if(ex==null){
+                System.out.println("Sent message=[" + message +
+                        "]with offset=[" + result.getRecordMetadata().offset()+"]");
+            }else{
+                System.out.println("Unable to send message=[" +
+                        message +"]due to :" + ex.getMessage());
+            }
+        });
+    }
+}
