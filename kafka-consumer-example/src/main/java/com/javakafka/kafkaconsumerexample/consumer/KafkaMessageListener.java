9b2d22ef4fe9b111295c47d3693af06f393e35c1
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-07-03 23:36:00 +0530

    reorganised the structure for this repo and added the kafka consumer and producer under one file

--- kafka-consumer-example/src/main/java/com/javakafka/kafkaconsumerexample/consumer/KafkaMessageListener.java
+++ kafka-consumer-example/src/main/java/com/javakafka/kafkaconsumerexample/consumer/KafkaMessageListener.java
@@ -1,5 +1,6 @@
 package com.javakafka.kafkaconsumerexample.consumer;
 
+import com.javakafka.kafkaconsumerexample.dto.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,24 +11,39 @@ public class KafkaMessageListener {
 
     Logger log = LoggerFactory.getLogger(KafkaMessageListener.class);
 
-    // type of the method would be same as that of the producer method because this method would consume that event
-   // In real World scenarios one should write multiple consumer code for same topic
-    // If there are a lot of messages from the producers there will be a lag (consumer doesnt consume the messages)
-    @KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
-    public void consume1(String message){
-        log.info("consumer1 consume the message{}" , message);
+    //
+    //This met
 
-    }@KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
-    public void consume2(String message){
-        log.info("consumer2 consume the message{}" , message);
-
-    }@KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
-    public void consume3(String message){
-        log.info("consumer3 consume the message{}" , message);
-
-    }@KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
-    public void consume4(String message){
-        log.info("consumer4 consume the message{}" , message);
+    @KafkaListener(topics="javatechieDemo",groupId = "jt-group")
+    public void consumeEvents(Customer customer){
+        log.info("consumer1 consume the message{}" , customer.toString());
 
     }
+
+
+
+    // type of the method would be same as that of the producer method because this method would consume that event
+    // In real World scenarios one should write multiple consumer code for same topic
+    // If there are a lot of messages from the producers there will be a lag (consumer doesnt consume the messages)
+
+//     @KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
+//    public void consume1(String message){
+//        log.info("consumer1 consume the message{}" , message);
+//
+//    }
+//    @KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
+//    public void consume2(String message){
+//        log.info("consumer2 consume the message{}" , message);
+//
+//    }
+//    @KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
+//    public void consume3(String message){
+//        log.info("consumer3 consume the message{}" , message);
+//
+//    }
+//    @KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
+//    public void consume4(String message){
+//        log.info("consumer4 consume the message{}" , message);
+//
+//    }
 }
