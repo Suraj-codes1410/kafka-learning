f7e6b63ba47600184b3e5d63f174344a44931aff
Suraj-codes1410 <samantasuraj1410@gmail.com>
2026-07-08 17:13:19 +0530

    Commented the the other Listener because wanted to play with DLT

--- kafka-consumer-example/src/main/java/com/javakafka/kafkaconsumerexample/consumer/KafkaMessageListener.java
+++ kafka-consumer-example/src/main/java/com/javakafka/kafkaconsumerexample/consumer/KafkaMessageListener.java
@@ -7,9 +7,11 @@ import com.javakafka.kafkacommon.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -37,10 +39,10 @@ public class KafkaMessageListener {
     // In real World scenarios one should write multiple consumer code for same topic
     // If there are a lot of messages from the producers there will be a lag (consumer doesnt consume the messages)
 
-     @KafkaListener(topics="java-partition-learn",groupId = "jt-group",topicPartitions = {@TopicPartition(topic="java-partition-learn",partitions = {"2"})})
-    public void consume1(String message){
-        log.info("consumer1 consume the message{}" ,message );
-    }
+//     @KafkaListener(topics="java-partition-learn",groupId = "jt-group",topicPartitions = {@TopicPartition(topic="java-partition-learn",partitions = {"2"})})
+//    public void consume1(String message){
+//        log.info("consumer1 consume the message{}" ,message );
+//    }
 //    @KafkaListener(topics="javatechie-demo1",groupId = "jt-group")
 //    public void consume2(String message){
 //        log.info("consumer2 consume the message{}" , message);
@@ -58,18 +60,33 @@ public class KafkaMessageListener {
 //    }
 
 
-    @KafkaListener(topics = "{app.topic.name}",groupId = "javatechie-group")
-    public void consumeEvents(User user , @Header(KafkaHeaders.RECEIVED_TOPIC) String topic , @Header(KafkaHeaders.OFFSET) long offset){
-         try{
-         log.info("Recieved : {} from {} offset {}",new ObjectMapper().writeValueAsString(user));
-             List<String> restrictedIpList = Stream.of("32.241.244.236","15.55.49.164","81.1.995.246").toList();
-             if(restrictedIpList.contains(user.getIpAddress())){
-                 throw new RuntimeException("Invalid IP Address recieved !");
-             }
+//    @KafkaListener(topics = "{app.topic.name}",groupId = "javatechie-group")
+//    public void consumeEvents(User user , @Header(KafkaHeaders.RECEIVED_TOPIC) String topic , @Header(KafkaHeaders.OFFSET) long offset){
+//         try{
+//         log.info("Recieved : {} from {} offset {}",new ObjectMapper().writeValueAsString(user));
+//             List<String> restrictedIpList = Stream.of("32.241.244.236","15.55.49.164","81.1.995.246").toList();
+//             if(restrictedIpList.contains(user.getIpAddress())){
+//                 throw new RuntimeException("Invalid IP Address recieved !");
+//             }
+//
+//         } catch (JsonProcessingException e) {
+//             throw new RuntimeException(e);
+//         }
+//    }
 
-         } catch (JsonProcessingException e) {
-             throw new RuntimeException(e);
-         }
+    @RetryableTopic(
+            attempts = "4",
+            backoff = @Backoff(delay = 5000)
+    )
+    @KafkaListener(
+            topics = "user-registration-topic",
+            groupId = "registration-group"
+    )
+    public void consume(User user){
+
+        System.out.println("Received : " + user);
+
+        throw new RuntimeException("SMTP Server Down");
     }
 
 
